5a2326292f4ea910a1a705c0be94defa8642e420
Scott Bass <scottbass47@gmail.com>
2020-08-03 22:24:00 -0400

    Path creation

--- src/Pathing.hs
+++ src/Pathing.hs
@@ -13,8 +13,7 @@ import Types
 data Path =
   Path
     { inputs :: [Input]
-    , start :: Tetromino
-    , end :: Tetromino -- if terminalPosition board end -> add to paths and dont put back in queue else 
+    , pathNodes :: [Tetromino]
     }
   deriving (Show)
 
@@ -26,10 +25,24 @@ data Node =
     }
   deriving (Show, Eq, Ord)
 
--- findPaths :: Board -> Tetromino -> [Path]
--- findPaths board tetromino = [Path result tetromino tetromino]
---   where
---     result = dfs board tetromino Set.empty
+findPaths :: Board -> Tetromino -> [Path]
+findPaths board tetromino = map (reversePath . nodeToPath) terminalNodes
+  where
+    (_, _, terminalNodes) =
+      bfs board (Q.singleton startNode) (S.singleton tetromino) []
+    startNode = Node Nothing Nop tetromino
+    --
+    nodeToPath :: Node -> Path
+    nodeToPath (Node Nothing _ tetromino) = Path [] [tetromino]
+    nodeToPath (Node (Just parent) input tetromino) =
+      Path (input : inputs parentPath) (tetromino : pathNodes parentPath)
+      where
+        parentPath = nodeToPath parent
+    --
+    reversePath :: Path -> Path
+    reversePath (Path inputs pathNodes) =
+      Path (reverse inputs) (reverse pathNodes)
+
 bfs ::
      Board
   -> Q.Seq Node
@@ -38,7 +51,7 @@ bfs ::
   -> (Q.Seq Node, S.Set Tetromino, [Node])
 bfs board toVisit visited endNodes
   | Q.null toVisit = (toVisit, visited, endNodes)
-  | otherwise = bfs board (insertAll tail moves) visited' newEndNodes
+  | otherwise = bfs board toVisit' visited' newEndNodes
   where
     head Q.:< tail = Q.viewl toVisit
     headT = tetromino head
@@ -49,6 +62,7 @@ bfs board toVisit visited endNodes
       fmap (fromJust . sequence) . filter ((/= Nothing) . snd) $
       nextNodes
     visited' = foldl' (flip S.insert) visited . map tetromino $ moves
+    toVisit' = foldl' (Q.|>) tail moves
     newEndNodes =
       if terminalPosition board headT
         then head : endNodes
@@ -65,9 +79,6 @@ bfs board toVisit visited endNodes
     tryPathMove root (Rotate dir) = tryRotate board root dir
     tryPathMove root Nop = Nothing
     tryPathMove root ForceDown = tryMove board root (0, -1)
-    --
-    insertAll :: Q.Seq Node -> [Node] -> Q.Seq Node
-    insertAll = foldl' (Q.|>)
 
 inputsToTry :: [Input]
 inputsToTry = [Move R, Move L, Rotate CW, Rotate CCW, ForceDown]
--- src/Playground.hs
+++ src/Playground.hs
@@ -24,3 +24,13 @@ runBfs = do
   let (_, _, nodes) = bfs mini (Q.singleton startNode) (S.singleton t) []
   sequence_ . map (putStrLn . show . flip placeTetromino mini . tetromino) $
     nodes
+
+runFindPaths :: IO ()
+runFindPaths = do
+  board <- canonIO
+  let t = moveTetromino (5, 15) tPiece
+  let paths = findPaths board t
+  let printBoard' = putStrLn . show . flip placeTetromino board . last
+  let printInputs = putStrLn . show
+  let printPath (Path inputs nodes) = printBoard' nodes >> printInputs inputs
+  sequence_ . map printPath $ paths
